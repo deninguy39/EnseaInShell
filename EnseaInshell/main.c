063156c246b8b4beaadf945a23a983a176b16459
nguyen denis <denis@MacBook-Pro-de-Denis.local>
2023-12-04 11:42:13 +0100

    q5

--- EnseaInshell/main.c
+++ EnseaInshell/main.c
@@ -22,7 +22,9 @@ int main(){
 
     while(1){
 
-        
+        struct timespec start, stop;
+        int time;
+
         write (STDOUT_FILENO,ENSEASH,strlen(ENSEASH));   
         int nbBytes=read(STDIN_FILENO,console_input,MAX_INPUT_SIZE);
 
@@ -34,6 +36,9 @@ int main(){
             exit(EXIT_SUCCESS); 
         }
         
+        //start of the clock here
+        clock_gettime( CLOCK_REALTIME, &start);
+            
 
         pid_t pid = fork();
 
@@ -45,7 +50,8 @@ int main(){
         //in the son process
         if(pid==0){ 
 
-            if (strcmp(console_input,"fortune")==0){
+        /*
+         if (strcmp(console_input,"fortune")==0){
                 execlp("fortune","fortune",NULL);
                 perror("execlp failed");
                 exit(EXIT_FAILURE);
@@ -56,7 +62,12 @@ int main(){
                 perror("execlp failed");
                 exit(EXIT_FAILURE);
             }   
+        */
 
+
+        execlp(console_input,console_input,NULL);
+        perror("execlp failed");
+        exit(EXIT_FAILURE);
     
         }
             
@@ -66,9 +77,12 @@ int main(){
 
             //get the status of the son when exited 
             if (WIFEXITED(son_status)) {
-                sprintf(buf, "enseash [exit:%d] %%\n", WEXITSTATUS(son_status));
+                clock_gettime( CLOCK_REALTIME, &stop);
+                time=(stop.tv_nsec-start.tv_nsec);
+                sprintf(buf, "enseash [exit:%d]|%dms %%\n", WEXITSTATUS(son_status),time/1000000); // the time is always not correct (always 0ms)
                 write(STDOUT_FILENO,buf,strlen(buf));
             } 
+
             //if the son process exit because of a signal
             else if (WIFSIGNALED(son_status)) {
                 sprintf(buf, "enseash [sign:%d] %%\n", WTERMSIG(son_status));
@@ -76,7 +90,5 @@ int main(){
             }
         }
     }
-    return 0;
+    return EXIT_SUCCESS;
     }  
-
-
