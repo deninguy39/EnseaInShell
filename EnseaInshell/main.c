9aaf3d2193f99ff2ac6d3754e84e5fc5df8bc619
nguyen denis <denis@MacBook-Pro-de-Denis.local>
2023-12-04 21:43:19 +0100

    q6

--- EnseaInshell/main.c
+++ EnseaInshell/main.c
@@ -18,77 +18,79 @@ int main(){
 
     write (STDOUT_FILENO,WELCOME_MSG,strlen(WELCOME_MSG));  
 
-    
+   
 
     while(1){
 
         struct timespec start, stop;
         int time;
+       
 
-        write (STDOUT_FILENO,ENSEASH,strlen(ENSEASH));   
+        write (STDOUT_FILENO,ENSEASH,strlen(ENSEASH));  
         int nbBytes=read(STDIN_FILENO,console_input,MAX_INPUT_SIZE);
 
         console_input[nbBytes-1]=0;
 
-        //in the case we want to stop 
+        //in the case we want to stop
         if (strcmp(console_input, "exit") == 0) {
-            write (STDOUT_FILENO,BYE,strlen(BYE)); 
-            exit(EXIT_SUCCESS); 
+            write (STDOUT_FILENO,BYE,strlen(BYE));
+            exit(EXIT_SUCCESS);
         }
-        
+       
         //start of the clock here
-        clock_gettime( CLOCK_REALTIME, &start);
-            
+        clock_gettime(CLOCK_REALTIME, &start);
+           
 
         pid_t pid = fork();
 
         if (pid==-1){
             perror("fork failed");
-            exit(EXIT_FAILURE);   
+            exit(EXIT_FAILURE);  
         }
 
         //in the son process
-        if(pid==0){ 
+        if(pid==0){
+            //arguments recovery
+            char splitter[]=" ";
+            int i = 0;
+            char **list_arg = NULL;
+            char *arg = strtok(console_input,splitter);
 
-        /*
-         if (strcmp(console_input,"fortune")==0){
-                execlp("fortune","fortune",NULL);
-                perror("execlp failed");
-                exit(EXIT_FAILURE);
-            }   
+             while (arg != NULL){
+                // memory allocation
+                list_arg = realloc(list_arg,(i+1)*sizeof(char *));
+                list_arg[i]= malloc(strlen(arg));
+                strcpy(list_arg[i],arg);
 
-            if (strcmp(console_input,"date")==0){
-                execlp("date","date",NULL);
-                perror("execlp failed");
-                exit(EXIT_FAILURE);
-            }   
-        */
+                arg = strtok(NULL,splitter);
+                i++;
+                }
+       
 
-
-        execlp(console_input,console_input,NULL);
-        perror("execlp failed");
-        exit(EXIT_FAILURE);
-    
-        }
-            
+            execvp(list_arg[0],list_arg);
+            free(list_arg);
+            perror("execvp failed");
+            exit(EXIT_FAILURE);
+            }
+           
         //in the parent process
         else{
             wait(&son_status);
 
-            //get the status of the son when exited 
-            if (WIFEXITED(son_status)) {
+            //get the status of the son when exited
+            if (WIFEXITED(son_status)){
                 clock_gettime( CLOCK_REALTIME, &stop);
                 time=(stop.tv_nsec-start.tv_nsec);
                 sprintf(buf, "enseash [exit:%d]|%dms %%\n", WEXITSTATUS(son_status),time/1000000); // the time is always not correct (always 0ms)
                 write(STDOUT_FILENO,buf,strlen(buf));
-            } 
+            }
 
             //if the son process exit because of a signal
-            else if (WIFSIGNALED(son_status)) {
+            else if (WIFSIGNALED(son_status)){
                 sprintf(buf, "enseash [sign:%d] %%\n", WTERMSIG(son_status));
                 write(STDOUT_FILENO,buf,strlen(buf));
             }
         }
     }
     return EXIT_SUCCESS;
-    } 
+    }
