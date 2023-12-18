3cdbd8094d33d51dd5d0eb9aeb15005c8b302d66
ensea <ensea@StudentLab>
2023-12-18 09:44:26 +0100

    correction q précédentes et q4

---
+++ tftp/mainq4.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <memory.h>
+#include <netdb.h>
+#include <sys/socket.h> 
+#include <string.h>
+
+int main(int argc, char ** argv){
+	if (argc !=4){
+		printf("Wrong usage \nPlease use %s hostname port file",argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	printf("Welcome to an implementation of a tftp client\n");
+	struct addrinfo hints;
+	struct addrinfo * result;
+	memset(&hints,0,sizeof(struct addrinfo));
+	hints.ai_protocol = IPPROTO_UDP;
+	hints.ai_family = AF_INET;
+	
+	int status;
+	status = getaddrinfo(argv[1],argv[2], &hints, &result);
+	if (status==-1){
+		printf("It seems like I can't find the host %s\n", argv[1]);
+	}
+	printf("Sending %s to the host : %s@%s\n", argv[3], argv[1], argv[2]);
+	
+	char RRQ[128]={0};
+	RRQ[1];
+	sprintf(RRQ+2,"%s", argv[3]);
+	sprintf(RRQ+strlen(argv[3])+3, "NETASCII");
+	int sd=socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	
+	sendto(sd, RRQ, strlen(argv[3])+12,0,res->ai_addr, res->ai_addrlen);
+	
+	int numberReadBytes;
+	char receivedBuffer[1024];
+	struct sockaddr socketReceived;
+	int socketReceiveSize;
+	numberReadBytes=recvfrom(sd,receivedBuffer,1024,0,&socketReceived,&socketReceiveSize);
+	
+	if (receivedBuffer[1]==5{
+		printf("Error code received from the server : \n%s\n", receivedBuffer+4);
+		exit(EXIT_SUCCESS);
+	}
+	
+	if(receivedBuffer[1]==3{
+		printf("I received %d bytes", numberReadBytes-4);
+		char ack[4]={0,4,0,0};
+		ack[3]=receivedBuffer[3];
+		sendto(sd,ack,4,0,&socketReceived,&socketReceiveSize);
+	}
+	
+	return 0;
+}
